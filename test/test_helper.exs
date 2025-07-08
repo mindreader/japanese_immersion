e2d8cff5ea1f828dd21bac70214e32dbd746bc28
David McHealy <david.mchealy@gmail.com>
2025-07-08 10:30:28 -0400

    chore: comment to guide ai.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,6 @@
 ExUnit.start()
+# I only want modules here that could cause side effects if used errantly.
+# all other copies should be made in the test file itself.
 Mimic.copy(Japanese.Corpus.StorageLayer)
 Mimic.copy(Anthropix)
 # Ecto.Adapters.SQL.Sandbox.mode(Japanese.Repo, :manual)
