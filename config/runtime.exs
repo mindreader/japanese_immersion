fef0c1819b35be792905b48ffbf9524bf222f829
David McHealy <david.mchealy@gmail.com>
2025-07-11 10:56:36 -0400

    chore: ditch db for now, and make flake release package

--- config/runtime.exs
+++ config/runtime.exs
@@ -21,20 +21,22 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+  # Not using a database for now, but perhaps in the future
+
+  # database_url =
+  #   System.get_env("DATABASE_URL") ||
+  #     raise """
+  #     environment variable DATABASE_URL is missing.
+  #     For example: ecto://USER:PASS@HOST/DATABASE
+  #     """
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
-  config :japanese, Japanese.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+  # config :japanese, Japanese.Repo,
+  #   # ssl: true,
+  #   url: database_url,
+  #   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  #   socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -48,7 +50,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || System.get_env("HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :japanese, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -64,56 +66,6 @@ if config_env() == :prod do
       port: port
     ],
     secret_key_base: secret_key_base
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :japanese, JapaneseWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :japanese, JapaneseWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  #     config :japanese, Japanese.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 end
 
 if config_env() != :test do
--- flake.nix
+++ flake.nix
@@ -8,11 +8,19 @@
   outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in with pkgs; {
-
-    # nix develop
+    lib = pkgs.lib;
+    pname = "japanese-immersion";
+    version = "0.1.0";
+    src = ./.;
+    mixFodDeps = pkgs.beamPackages.fetchMixDeps {
+      inherit pname version src;
+      hash = "sha256-JLyjcFBlixlyhLobbPf/eloYS5MeKfGMLHpaFvXT3T8=";
+    };
+    japanese-immersion-release = pkgs.beamPackages.mixRelease {
+      inherit pname version src mixFodDeps;
+    };
+  in {
     devShells.x86_64-linux.default = pkgs.mkShell {
-
       name = "elixir-shell";
       packages = with pkgs; [
         pkgs.beam.packages.erlang_27.elixir_1_18
@@ -21,5 +29,7 @@
         inotify-tools
       ];
     };
+    packages.x86_64-linux.default = japanese-immersion-release;
+    packages.x86_64-linux.japanese-immersion-release = japanese-immersion-release;
   };
 }
